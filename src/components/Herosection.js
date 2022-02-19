e64ccd4e8c6f60258d32b7c7faff6a09d81274c8
AtoBrightSide <meseleabel99@gmail.com>
2022-02-19 11:51:14 +0300

    finished with the nav, and started with the footer

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
+import { Carousel } from 'react-bootstrap';
 
 const Hero = styled.div`
     background-color: linear-gradient(45deg, blue, red);
     width: 100%;
-    height: max-content;
+    height: fit-content;
 `;
 
 
 export default function Herosection() {
     return (
         <Hero>
-            <p>This is the herosection, its going to be in the middle of the  homepage</p>
+            This is the herosection
         </Hero>
     );
 }
