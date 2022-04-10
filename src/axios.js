9b054272fef4abaab9519ebf74be2d01f73a9a4d
Barok Dagim <61631544+BarokDG@users.noreply.github.com>
2022-04-11 00:31:02 +0300

    Fix api error

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://getzauto.herokuapp.com";
+const baseURL = "https://getzauto.herokuapp.com";
 const instance = axios.create({
   baseURL,
   headers: {
