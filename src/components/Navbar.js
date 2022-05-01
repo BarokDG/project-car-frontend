31466c67b237e005e2bb1a2192fbaaf497c9dfa4
Nah7wzy <tamnahom7@gmail.com>
2022-05-01 22:28:37 +0300

    minor fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
-import { Nav, Logo, MenuLink } from "../styles/Navbar.style";
+import { useState } from "react";
+
+import { Nav, Logo, Hamburger, Menu, MenuLink } from "../styles/Navbar.style";
 
 export default function Navbar({ openInfoModal }) {
+  const [isOpen, setisOpen] = useState(true);
+
   return (
     <Nav>
       <Logo href="/">
         Getz<span>Automotive</span>
       </Logo>
-      <MenuLink onClick={openInfoModal}>Want to sell a car?<br/>እዚህ ይጫኑ</MenuLink>
+      {/* <Hamburger onClick={() => setisOpen(!isOpen)}>
+        <span />
+        <span />
+        <span />
+      </Hamburger> */}
+      <Menu isOpen={isOpen}>
+        <MenuLink onClick={openInfoModal}> መኪና መሸጥ ይፈልጋሉ? <br/>እዚህ ይጫኑ</MenuLink>
+      </Menu>
     </Nav>
   );
 }
